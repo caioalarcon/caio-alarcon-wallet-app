46080fc2bac66968a9f4ff60f760de495543c94a
caioalarcon <42846850+caioalarcon@users.noreply.github.com>
2025-11-30 20:33:32 -0300

    Add logging interceptor dependency for core-data tests

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
 
 kapt {
